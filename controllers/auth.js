bbd502d53a70cbc44c00ab92991e08ae83e748ed
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2022-02-06 21:48:48 +0700

    code controller auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,19 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const access_token = req.headers["authorization"].split(" ")[1];
+        const user = jwt.verify(access_token, config.AUTH_TOKEN_SECRET.ACCESS_TOKEN);
+        if (user){
+            req.user = user;
+            return next();
+        }
+        return res.status(401).json({message: 'user not exist'});
+    }catch (err) {
+        return res.status(401).json(err);
+    }
+}
 const createUser = async (req, res) => {
     try {
         const {email,first_name,last_name,phone,address,gender,password} = req.body;
@@ -67,7 +80,45 @@ const signAccessToken = (user_id) => {
         }
     );
 }
+const changePassword = async (req, res) => {
+    try{
+        const user_id = req.user.id;
+        const {old_password,password} = req.body;
+        const user = await Users.findByPk(user_id);
+        if (!old_password || !password) {
+            return res.status(400).json("Incorrect information");
+        }else {
+            const isCorrectPass = await bcrypt.compare(old_password, user.password);
+            if (!isCorrectPass){
+                return res.status(400).json({message: "Incorrect password"});
+            }else {
+                const hashPassword = await bcrypt.hash(password,10);
+                await Users.update({
+                    password: hashPassword,
+                },{
+                    where: {id: user_id}
+                });
+                return res.status(200).json({success: true});
+            }
+        }
+    }catch (err){
+        return res.status(400).json(err);
+    }
+}
+const refreshTokenUser = async (req, res) => {
+    try {
+        const {token} = req.body;
+        const user = jwt.verify(token,config.AUTH_TOKEN_SECRET.REFRESH_TOKEN);
+        const accessToken = signAccessToken(user.id);
+        return res.status(200).json({accessToken: accessToken});
+    }catch (err) {
+        return res.status(400).json({message: err.toString()});
+    }
+}
 
 module.exports = {
     createUser,
+    loginUser,
+    refreshTokenUser,
+    changePassword
 }
