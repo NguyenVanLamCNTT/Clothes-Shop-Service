1f4569844d4c1796e339384f0f341e8f0357dda5
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2022-02-09 15:00:35 +0700

    import cloudinary

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 const db = require('./db')
 const container = require('./container')
 const auth = require('./auth')
+const cloudinary = require('./clouddinary')
 module.exports = {
     ...db,
     ...container,
-    ...auth
+    ...auth,
+    ...cloudinary
 }
